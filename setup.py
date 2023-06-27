d4cf9d2606b2aedd5e8358c1ea7964b58cc59a2b
Jakub Kwiatkowski <jkwiatkowski@man.poznan.pl>
2023-06-27 18:45:18 +0200

    feat: Add setup.py.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='core_tools',
+    version='',
+    packages=['core_tools'],
+    url='',
+    license='MIT',
+    author='Jakub Kwiatkowski',
+    author_email='jakubkwiatkowski1994@gmail.com',
+    description=''
+)
